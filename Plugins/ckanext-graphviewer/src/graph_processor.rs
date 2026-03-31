59ab3b8fce44ce133bfebb928322c243dc9a167c
Wolfgang Schröder <wolfgang.schroeder@stud.uni-hannover.de>
2026-03-31 14:53:56 +0200

    expanded inti center node

--- Plugins/ckanext-graphviewer/src/graph_processor.rs
+++ Plugins/ckanext-graphviewer/src/graph_processor.rs
@@ -180,7 +180,7 @@ pub fn build_ui_graph(triples: Vec<RawTriple>) -> (Vec<Node>, Vec<Edge>) {
         visited.insert(start_node.clone());
 
         let mut queue = VecDeque::new();
-        queue.push_back((start_node, root_pos, 250.0, 0.0, std::f32::consts::TAU));
+        queue.push_back((start_node.clone(), root_pos, 250.0, 0.0, std::f32::consts::TAU));
 
         while let Some((curr_id, parent_pos, radius, start_angle, end_angle)) = queue.pop_front() {
             if let Some(children) = adjacency.get(&curr_id) {
@@ -295,5 +295,17 @@ pub fn build_ui_graph(triples: Vec<RawTriple>) -> (Vec<Node>, Vec<Edge>) {
             });
         }
     }
+
+    // expand center dataset block
+    if let Some(&root_idx) = id_to_index.get(&start_node) {
+        nodes[root_idx].expanded = true;
+        for edge in &mut edges {
+            if edge.source == root_idx {
+                edge.visible = true;
+                nodes[edge.target].visible = true;
+            }
+        }
+    }
+
     (nodes, edges)
 }
